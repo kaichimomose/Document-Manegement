fbdeae608f82f0ea9e67dbf3ef8c8f293eb6e0bb
kaichimomose <30785433+kaichimomose@users.noreply.github.com>
2018-01-19 11:35:29 -0800

    Initial Commit

---
+++ Document-Management/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Document-Management
+//
+//  Created by Kaichi Momose on 2018/01/19.
+//  Copyright © 2018 Kaichi Momose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
